f1f35412e8a63d77b334993681689dd2c2f1bf43
PHIL528 <philippaulmurray@gmail.com>
2020-06-30 00:17:52 +0200

    need to check something

--- server/main.go
+++ server/main.go
@@ -5,16 +5,39 @@ import (
 	"log"
 	"net"
 	"proto-playground/proto"
-
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"cloud.google.com/go/pubsub"
 )
 
 func main() {
+	os.Setenv("PUBSUB_EMULATOR_HOST", "localhost:8085")
+	ps_ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, proj)
+	var client *pubsub.Client; var err_mem error
+	for {
+		client, err := pubsub.NewClient(ps_ctx, "karhoo-local")
+		if err == nil {
+			break
+		} else if err != err_mem {
+			log.Fatalf("Failed to create pubsub client %v", err)
+			log.Fatalf("Will reattempt every 0.5 seconds")
+			err_mem = err
+		}
+		time.Sleep(time.Second * 0.5)
+	}
+	var topic *Topic; err_mem = nil
+	for {
+		topic, err 
+	}
+
+
 	lis, err := net.Listen("tcp", ":3001") //React port + 1
+
 	if err != nil {
 		log.Fatalf("Error %v", err)
 	}
+	client, 
 	gcrpServer := grpc.NewServer()
 	//proto generates ReservationService Server
 	proto.RegisterReservationServiceServer(gcrpServer, &server{})
@@ -26,11 +49,11 @@ func main() {
 
 }
 
-type server struct{}
+type server struct {}
 
 func (s *server) MakeReservation(ctx context.Context, req *proto.BookTrip) (*proto.Trip, error) {
 	log.Printf("Recieved request to book trip from client %s", req.PassengerName)
-	new_trip := proto.Trip{
+	new_trip := proto.Trip {
 		PassengerName: req.PassengerName,
 		DriverName:    "Marek",
 	}
