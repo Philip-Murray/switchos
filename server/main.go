0634c33c1a8b5e54b7d6c7eb176fb21802e3c937
PHIL528 <philippaulmurray@gmail.com>
2020-06-30 02:32:45 +0200

    smgo

--- server/main.go
+++ server/main.go
@@ -2,58 +2,68 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net"
+	"os"
 	"proto-playground/proto"
+	"time"
+
+	"cloud.google.com/go/pubsub"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-	"cloud.google.com/go/pubsub"
 )
+
+var topic *pubsub.Topic
 
 func main() {
 	os.Setenv("PUBSUB_EMULATOR_HOST", "localhost:8085")
 	ps_ctx := context.Background()
-	client, err := pubsub.NewClient(ctx, proj)
-	var client *pubsub.Client; var err_mem error
+	notified := false
+	var client *pubsub.Client
+	var err error
 	for {
-		client, err := pubsub.NewClient(ps_ctx, "karhoo-local")
+		client, err = pubsub.NewClient(ps_ctx, "karhoo-local")
 		if err == nil {
 			break
-		} else if err != err_mem {
+		} else if !notified {
 			log.Fatalf("Failed to create pubsub client %v", err)
-			log.Fatalf("Will reattempt every 0.5 seconds")
-			err_mem = err
+			log.Fatalf("Will reattempt every second to connect to PubSub, please start PubSub terminal")
+			notified = true
 		}
-		time.Sleep(time.Second * 0.5)
+		time.Sleep(time.Second)
 	}
-	var topic *Topic; err_mem = nil
-	for {
-		topic, err 
+	topic, err = client.CreateTopic(ps_ctx, "events.TripBooked")
+	if err != nil {
+		log.Fatalf("Failed to create topic %v", err)
+		panic()
 	}
-
+	defer topic.Stop()
 
 	lis, err := net.Listen("tcp", ":3001") //React port + 1
-
 	if err != nil {
 		log.Fatalf("Error %v", err)
 	}
-	client, 
 	gcrpServer := grpc.NewServer()
 	//proto generates ReservationService Server
 	proto.RegisterReservationServiceServer(gcrpServer, &server{})
 	reflection.Register(gcrpServer)
-
 	if err := gcrpServer.Serve(lis); err != nil {
 		log.Fatalf("Error %v", err)
 	}
-
 }
 
-type server struct {}
+type server struct{}
 
 func (s *server) MakeReservation(ctx context.Context, req *proto.BookTrip) (*proto.Trip, error) {
 	log.Printf("Recieved request to book trip from client %s", req.PassengerName)
-	new_trip := proto.Trip {
+	ps_ctx := context.Background()
+
+	if _, err := topic.Publish(ps_ctx, &pubsub.Message{Data: []byte("payload")}).Get(ps_ctx); err != nil {
+		log.Fatalf(err)
+		return &proto.Trip{}, errors.New("Could not confirm reservation %v", err)
+	}
+	new_trip := proto.Trip{
 		PassengerName: req.PassengerName,
 		DriverName:    "Marek",
 	}
